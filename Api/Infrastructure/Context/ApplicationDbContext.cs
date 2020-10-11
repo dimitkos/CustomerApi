2c946c0baed337bec745d9f0c320ee8efa60365b
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-11 22:12:44 +0300

    Add dbcontext

---
+++ Api/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
